3a7171d8fea05d4fd637e16626c3675a79ae6078
Atul Mahankal <atulmahankal@gmail.com>
2024-01-08 18:29:23 +0530

    update

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const express = require('express');
 const endPoints = require('express-list-endpoints');
 const portscanner = require('portscanner');
 const http = require('http');
+const moment = require('moment');
 
 //import middlewares
 const bodyParser = require('body-parser');
@@ -27,8 +28,12 @@ dotenv.config(process.cwd());
 process.env.DEBUG = 'expressbooster:*'
 const debug = require('debug')('expressbooster:server');
 
-const port = process.env.APP_PORT || 3000;
-const config = importModules(path.join(process.cwd(), 'config'));
+const port = normalizePort(process.env.APP_PORT || '3000');
+const env = process.env.NODE_ENV || process.env.APP_ENV || 'development';
+const isProduction = (env === 'production')
+const isDev = (env === 'development')
+
+const configs = importModules(path.join(process.cwd(), 'config'));
 const models = importModules(path.join(process.cwd(), 'models'));
 
 class Server extends EventEmitter {
@@ -36,67 +41,7 @@ class Server extends EventEmitter {
     super();
     this.app = express();
 
-    // Use Middleware
-    this.app.use(express.urlencoded({ extended: false }));
-    this.app.use(express.json());
-    this.app.use(express.static(path.join(process.cwd(), 'public'), options));
-    this.app.use(bodyParser.json({ extended: false }));
-    this.app.use(cookieParser());
-    this.app.use(helmet({ poweredBy: false }));
-    this.app.use(cors());
-    this.app.use(compression({ filter: shouldCompress }))
-    this.app.use(
-      methodOverride(function (req) {
-        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-          // look in urlencoded POST bodies and delete it
-          var method = req.body._method;
-          delete req.body._method;
-          return method;
-        }
-      })
-    );
-
-    // Set up Winston logger
-    const transports = [
-      new winston.transports.Console(),
-    ];
-    
-    // Conditionally add File transport if in production
-    if (isProduction) {
-      transports.push(new winston.transports.File({
-        filename: path.join(process.cwd(), 'logs', `request-${moment().format('YYYY-MM-DD')}.log`),
-      }));
-    }
-    
-    const requestLog = winston.createLogger({
-      transports: transports,
-      format: winston.format.combine(
-        winston.format.simple()
-      ),
-    });
-    
-    // Define a custom token for morgan using moment.js
-    morgan.token('date', () => {
-      return moment().format('YYYY-MM-DD hh:mm:ss A');
-    });
-    
-    // Don't log during tests
-    // Logging middleware
-    const morganMiddleware = morgan('combined', { stream: { write: message => requestLog.info(message.trim()) } });
-    if (env !== 'test' || env !== 'testing') this.app.use(morganMiddleware);
-    
-    if (fs.existsSync(path.join(process.cwd(), 'public', 'favicon.ico'))) {
-      this.app.use(favicon(path.join(process.cwd(), 'public', 'favicon.ico')))
-    }
-    
-    // Logs all request paths and method
-    this.app.use(function (req, res, next) {
-      const currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
-      res.set('x-timestamp', currentDateTime)
-      // res.set('x-powered-by', 'cyclic.sh')
-      // console.log(`[${currentDateTime}] ${req.ip} ${req.method} ${req.path} ${req.get('User-Agent')}`);
-      next();
-    });
+    addMiddlewares(this.app);
 
     if (process.env.APP_ENV === 'development') {
       setupAllUrlsRoute(this.app, this);
@@ -132,12 +77,127 @@ class Server extends EventEmitter {
   }
 
   use(middleware) {
+    this.app.use(reqMiddleware);
+    this.app.use(resMiddleware);
     this.app.use(middleware);
-  }
-  app() {
-    return this.app;
+    this.app.use(notFoundMiddleware);
+    this.app.use(errMiddleware);
   }
 };
+
+function reqMiddleware(req, res, next) {
+  // console.log("========== reqMiddleware");
+  next();
+}
+
+function resMiddleware(req, res, next) {
+  // console.log("========== resMiddleware");
+  next();
+}
+
+// Error-handling middleware
+function notFoundMiddleware(req, res, next) {
+  res.status(404);
+  const error = new Error(`Url '${req.originalUrl}' not found!`);
+  next(error);
+};
+
+function errMiddleware(err, req, res, next) {
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.write(`${err.message}\n`);
+  res.write(`\n`);
+  if(isDev){
+    res.write(err.stack);
+  }
+  res.end();
+}
+
+function addMiddlewares(app) {
+  // Add Middleware
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  app.use(bodyParser.json({ extended: false }));
+  app.use(cookieParser());
+  app.use(helmet({ poweredBy: false }));
+  app.use(cors());
+
+  configs.express?.paths?.forEach(item => {
+    app.use(express.static(item.url, item.options || {}));
+  });
+  // app.use(express.static('images', {}));
+  app.use(compression({
+    filter: (req, res) => {
+      if (req.headers['x-no-compression']) {
+        // don't compress responses with this request header
+        return false
+      }
+      // fallback to standard filter function
+      return compression.filter(req, res)
+    }
+  }))
+
+  app.use(
+    methodOverride(function (req) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+      }
+    })
+  );
+
+  // Set up Winston logger
+  const transports = [
+    new winston.transports.Console(),
+  ];
+
+  // Conditionally add File transport if in production
+  if (isProduction) {
+    transports.push(new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', `request-${moment().format('YYYY-MM-DD')}.log`),
+    }));
+  }
+
+  const requestLog = winston.createLogger({
+    transports: transports,
+    format: winston.format.combine(
+      winston.format.simple()
+    ),
+  });
+
+  // Define a custom token for morgan using moment.js
+  morgan.token('date', () => {
+    return moment().format('YYYY-MM-DD hh:mm:ss A');
+  });
+
+  // Don't log during tests
+  // Logging middleware
+  const morganMiddleware = morgan('combined', { stream: { write: message => requestLog.info(message.trim()) } });
+  if (env !== 'test' || env !== 'testing') app.use(morganMiddleware);
+
+  if (fs.existsSync(path.join(process.cwd(), 'public', 'favicon.ico'))) {
+    app.use(favicon(path.join(process.cwd(), 'public', 'favicon.ico')))
+  }
+
+  // Logs all request paths and method
+  app.use(function (req, res, next) {
+    const currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+    res.set('x-timestamp', currentDateTime)
+    // res.set('x-powered-by', 'cyclic.sh')
+    // console.log(`[${currentDateTime}] ${req.ip} ${req.method} ${req.path} ${req.get('User-Agent')}`);
+    next();
+  });
+}
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  return isNaN(port) ? val : (port >= 0 ? port : false);
+}
 
 async function isPortInUse() {
   return await portscanner.checkPortStatus(port) === 'open';
@@ -217,34 +277,34 @@ function importModules(directoryPath) {
 }
 
 const logger = winston.createLogger({
-	transports: [
-		new winston.transports.Console(),
-		new winston.transports.File({
-			filename: path.join(
-				process.cwd(),
-				'logs',
-				`log${isProduction ? '-' + moment().format('YYYY-MM-DD') : ''}.log`
-			),
-		})
-	],
-	format: winston.format.combine(
-		winston.format.printf(info => {
-			const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
-			if (isDev) {
-				notification({
-					icon: 'Terminal Icon',
-					title: info.level.toUpperCase(),
-					message: info.message
-				})
-			}
-			return `${timestamp} [${info.level.toUpperCase()}] ${info.message}`;
-		})
-	),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        `log${isProduction ? '-' + moment().format('YYYY-MM-DD') : ''}.log`
+      ),
+    })
+  ],
+  format: winston.format.combine(
+    winston.format.printf(info => {
+      const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+      if (isDev) {
+        notification({
+          icon: 'Terminal Icon',
+          title: info.level.toUpperCase(),
+          message: info.message
+        })
+      }
+      return `${timestamp} [${info.level.toUpperCase()}] ${info.message}`;
+    })
+  ),
 });
 
 module.exports = {
   Server,
-  config,
+  configs,
   models,
   logger
 };
