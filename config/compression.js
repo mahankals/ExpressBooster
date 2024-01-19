0b805a8317c5fb5dd1e5cf80efbc5c6ca867b931
Atul Mahankal <atulmahankal@gmail.com>
2024-01-19 18:46:25 +0530

    update

--- config/compression.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-	if (req.headers['x-no-compression']) {
-		// don't compress responses with this request header
-		return false
-	}
-	// fallback to standard filter function
-	return compression.filter(req, res)
-};
